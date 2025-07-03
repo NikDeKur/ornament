18d389c9868376da84356dbaa142e76a8a14bc64
NikDeKur <125550021+NikDeKur@users.noreply.github.com>
2025-07-04 00:55:54 +0100

    Account, Authorization, Session services removed.

--- src/commonMain/kotlin/dev/nikdekur/ornament/account/AccountCredentials.kt
+++
@@ -1,9 +0,0 @@
-package dev.nikdekur.ornament.account
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-public data class AccountCredentials(
-    val login: String,
-    val password: String
-)
